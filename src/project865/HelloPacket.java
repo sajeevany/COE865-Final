e887beacff78d959a8cd24eedb63dcafccd211e0
Sajeevan <sajeevanyogeswaran@gmail.com>
2016-03-27 13:31:38 -0400

    Refactor + type change
    -Refactored for readability
    -Changed myAddress type from InetAddress to String. InetAddress handle
    is unnecessary and excessive for a hello packet.
    -Changed mySocket type from DatagramSocket to int. DatagramSocket handle
    is unnecessary and excessive for a hello packet.

--- src/project865/HelloPacket.java
+++ src/project865/HelloPacket.java
@@ -2,9 +2,7 @@ package project865;
 import java.io.Serializable;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HelloPacket implements Serializable {
@@ -12,34 +10,26 @@ public class HelloPacket implements Serializable {
 	private String myUniqueID = null;
 	// uniqueID, resource
 	private Map<String, String> uniqueIDResourceMap = new HashMap<String, String>();
-	private InetAddress myAddresses;
-	private DatagramSocket mySockets;
+	private String myAddress;
+	private int mySocket;
 
 	public static final int sendInterval = 10;
 	public static final int timeout = 3 * sendInterval;
 
-	public HelloPacket(String uniqueID,Map<String, String> uniqueIDResourceMap, InetAddress myAddresses, DatagramSocket mySockets)
-	{
-		//if (myAddresses.length != mySockets.length)
-		//{
-		//	throw new IllegalArgumentException("Number of addresses and sockets do not match");
-		//}
-		
+	public HelloPacket(String uniqueID,Map<String, String> uniqueIDResourceMap, InetAddress myAddress, DatagramSocket mySocket)
+	{	
 		this.myUniqueID = uniqueID;
 		this.uniqueIDResourceMap = uniqueIDResourceMap;
-		this.myAddresses = myAddresses;
-		this.mySockets = mySockets;
+		this.myAddress = myAddress.getHostAddress();
+		this.mySocket = mySocket.getPort();
 	}
 	
 	public String toString()
 	{
 		StringBuilder sB = new StringBuilder("myUniqueID: " + myUniqueID + "\n");
 		
-		//for (int i = 0; i < myAddresses.length; i++)
-		//{
-			sB.append("InetAddr" + ": "  + myAddresses.getHostAddress() + "\n");
-			sB.append("DatagramSockets" + ": "  + mySockets.getLocalPort() + "\n");
-		//}
+		sB.append("InetAddr" + ": "  + myAddress + "\n");
+		sB.append("DatagramSockets" + ": "  + mySocket + "\n");
 				
 		sB.append("\nResource List\n");
 		
