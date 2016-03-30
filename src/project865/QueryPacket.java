4f637e12618029bbc1e3ef428518c8a04b2362fe
Sajeevan <sajeevanyogeswaran@gmail.com>
2016-03-30 19:32:20 -0400

    QueryPacket changes

--- src/project865/QueryPacket.java
+++ src/project865/QueryPacket.java
@@ -6,60 +6,48 @@ import java.util.Map;
 public class QueryPacket implements Serializable {
 
 	private String myUniqueID = null;
-	private String myQueryID = null;
+	private String myDestinationID = null;
 	// if false then results being returned 
-	private Boolean direction = false;
-	private String results = null;
+	private boolean isRequest = false;
+	private String requestedFileName = null;
+	private boolean requestResult = false; //true - DestinationID has requestedFileName
 
 
-	public QueryPacket(String uniqueID, String queryID)
+	public QueryPacket(String uniqueID, String myDestinationID, boolean isRequest, String fileName, boolean requestedResponse)
 	{	
 		this.myUniqueID = uniqueID;
-		this.myQueryID = queryID;
-		this.direction = false;
-		this.setResults(null);
-		
+		this.myDestinationID = myDestinationID;
+		this.isRequest = isRequest;
+		this.requestedFileName = fileName;
+		this.requestResult  = requestedResponse;		
+	}
+
+
+	public String getMyUniqueID() {
+		return myUniqueID;
+	}
+
+
+	public String getMyDestinationID() {
+		return myDestinationID;
+	}
+
+
+	public boolean isRequest() {
+		return isRequest;
+	}
+
+
+	public String getRequestedFileName() {
+		return requestedFileName;
+	}
+
+
+	public boolean isRequestResult() {
+		return requestResult;
 	}
 	
-    @Override
-	public String toString()
-	{
-		StringBuilder sB = new StringBuilder("myUniqueID: " + getMyUniqueID() + "\n");
-		sB.append("QueryID" + ": "  + getMyQueryID() + "\n");
-				
-		return sB.toString();
-	}
-
-	/**
-     * @return the myQueryID
-     */
-    private String getMyQueryID() {
-		
-		return myQueryID;
-	}
-
-	/**
-     * @return the myUniqueID
-     */
-    public String getMyUniqueID() {
-        return myUniqueID;
-    }
-
-	private Boolean getDirection() {
-		return direction;
-	}
-
-	private void setDirection(Boolean newValue ) {
-		this.direction = newValue;
-	}
-
-	private String getResults() {
-		return results;
-	}
-
-	private void setResults(String results) {
-		this.results = results;
-	}
+    
 
 }
 
