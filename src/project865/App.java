9fe2c8faafc262ab3861d9d87268ebb7bd74247b
arjunm13 <arjunmahen13@gmail.com>
2016-03-31 12:38:46 -0400

    added some comments

--- src/project865/App.java
+++ src/project865/App.java
@@ -18,11 +18,17 @@ import java.util.StringTokenizer;
 
 public class App {
 
+	// Unique ID of host
 	private String myUniqueID = null;
+	// Resources belonging to host,
 	private String resourceNames[];
+	//Hello manager to run Hello protocol threads
 	private HelloManager hManager;
+	//Hello manager to run Query protocol threads
 	private QueryManager qManager;
+	// Hosts Ip Addresses and associted port #'s
 	private Map<String, Integer> myIPSocketMap = new HashMap<String, Integer>();
+	// targetMachines IP addresses to be read from Config File
 	private ArrayList<InetAddress> targetMachineIPs = new ArrayList<InetAddress>();
 
 	public App(String[] myIPs, int[] myQuerySockets, String[] myResourceList, String uniqueID, String configFileName) throws IOException {
@@ -30,6 +36,7 @@ public class App {
 			myIPSocketMap.put(myIPs[i], myQuerySockets[i]);
 		}
 
+		//set hosts values from constructor parameters
 		this.myUniqueID = uniqueID;
 		this.resourceNames = myResourceList;
 		
@@ -80,71 +87,33 @@ public class App {
                     br.close();
 		}	
 		
-		//Set<SourceTargetIPPair> stSet = new LinkedHashSet<SourceTargetIPPair>();
-		//this.hManager = new HelloManager(this.myUniqueID, sourceTargetIPMap, this.myIPSocketMap, myResourceList);
+		//		//Set<SourceTargetIPPair> stSet = new LinkedHashSet<SourceTargetIPPair>();		//this.hManager = new HelloManager(this.myUniqueID, sourceTargetIPMap, this.myIPSocketMap, myResourceList);
+
+
+		// Start Hello Manager and pass in UniqueID and directly connected ip addresses
+		// 
 		this.hManager = new HelloManager(this.myUniqueID, stIPPairList, this.myIPSocketMap, myResourceList);
 		this.hManager.runManager();
-                                
+        
+        // Init Query Manager for diffreent machines                        
                 //R1
-		//this.qManager = new QueryManager("R1",new String[]{"10.1.1.13"}, new int[] {10901}, new String[] {"r1File.txt","r1File2.txt" });
+				//this.qManager = new QueryManager("R1",new String[]{"10.1.1.13"}, new int[] {10901}, new String[] {"r1File.txt","r1File2.txt" });
                 //R2
                 //this.qManager = new QueryManager("R2",new String[]{"10.1.1.11", "10.1.2.10"}, new int[] {10902, 10903}, new String[] {"r2File.txt","r2File2.txt"});
                 //R3
                //this.qManager = new QueryManager("R3",new String[]{"10.1.2.7"}, new int[] {10904}, new String[]{"r3File.txt","r3File2.txt","r3File3.txt" });
+		
+		/// run the hello manager
 		this.qManager.runManager();
 		
 
 	}
 		
+	// Main function creates an instance of the App, Host IP and ports need to be specified
+	// Target Machines need to be set in Config File 
 	public static void main(String[] args) throws IOException {
 	
-//                String whoami = "R2";
-//            
-//                HashMap<String, ArrayList<String>> hostResourceMap = new HashMap<String, ArrayList<String>>();
-//                hostResourceMap.put("R1", new ArrayList<String>(){{add("r1File.txt"); add("r1File2.txt");}});
-//                hostResourceMap.put("R2", new ArrayList<String>(){{add("r2File.txt"); add("r2File2.txt");}});
-//                hostResourceMap.put("R3", new ArrayList<String>(){{add("r3File.txt"); add("r3File2.txt"); add("r3File3.txt");}});
-//                        
-//                ArrayList<AttributeTrio> rte1Tr = new ArrayList<AttributeTrio>();
-//                AttributeTrio atr1 = new AttributeTrio("R1", hostResourceMap, new ArrayList<String>(){{add("R2");}});
-//                AttributeTrio atr2 = new AttributeTrio("R2", hostResourceMap, new ArrayList<String>(){{add("R1"); add("R2");}});
-//                AttributeTrio atr3 = new AttributeTrio("R3", hostResourceMap, new ArrayList<String>(){{add("R2");}});
-//                rte1Tr.add(atr1);
-//                rte1Tr.add(atr2);
-//                rte1Tr.add(atr3);
-//                
-//                //R1
-//                //{who data is from, port to sendIQueryRequestsTo, what their uniqueID is}
-//                RoutingTableEntry rte1 = new RoutingTableEntry("10.1.1.13", 10901, "R1", rte1Tr); //local data
-//                RoutingTableEntry rte2 = new RoutingTableEntry("10.1.1.11", 10902, "R1", rte1Tr);
-//                
-//                //R2
-//                RoutingTableEntry rte3 = new RoutingTableEntry("10.1.1.11", 10902, "R2", rte1Tr); //local data
-//                RoutingTableEntry rte4 = new RoutingTableEntry("10.1.1.13", 10901, "R1", rte1Tr);
-//                RoutingTableEntry rte5 = new RoutingTableEntry("10.1.1.10", 10903, "R2", rte1Tr); //local data
-//                RoutingTableEntry rte6 = new RoutingTableEntry("10.1.2.7" , 10901, "R2", rte1Tr);
-//                
-//                //R3
-//                RoutingTableEntry rte7 = new RoutingTableEntry("10.1.2.7" , 10904, "R3", rte1Tr); //local data
-//                RoutingTableEntry rte8 = new RoutingTableEntry("10.1.1.10", 10903, "R2", rte1Tr);
-//		
-//                if (whoami.toUpperCase().equals("R1"))
-//                {
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte1);
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte2);
-//                }
-//                else if (whoami.toUpperCase().equals("R2"))
-//                {
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte3);
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte4);
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte5);
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte6);
-//                }
-//                else
-//                {
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte7);
-//                    RoutingTable.getRoutingTableInstance().addRoute(rte8);
-//                }
+
                 
                // App client1 = new App(new String[]{"10.1.1.13"}, new int[] {10901}, new String[] {"r1File.txt","r1File2.txt" }, "R1", "config");
                 App client2 = new App(new String[]{"10.1.1.11", "10.1.2.10"}, new int[] {10902, 10903}, new String[] {"r2File.txt","r2File2.txt"}, "R2", "config");
@@ -152,11 +121,14 @@ public class App {
                 
 	}
 
+	// Returns the network of the IP, Assuming mask = 24 bits
 	public static String getNetwork(String IP)
 	{
+		// Seperate IP by '.'
 		StringTokenizer sTok = new StringTokenizer(IP,".");
 		StringBuilder sBuild = new StringBuilder(); 
 		
+		// create network value by loading value and adding 0 as last byte
 		for (int j = 0; j < sTok.countTokens(); j++ )
 		{
 			sBuild.append(sTok.nextToken() + ".");
